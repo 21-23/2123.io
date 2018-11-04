93fda04343557559a4b14bfd734b031900bf8315
Kiril Knysh <k.knysh@gmail.com>
2018-11-04 15:38:47 +0100

    Use qd-front in local;

---
+++ webpack.config.local.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const webpackMerge = require('webpack-merge');
+
+const baseConfig = require('./webpack.config.base.js');
+
+
+module.exports = function(env) {
+    return webpackMerge(baseConfig, {
+        output: {
+            path: path.resolve(__dirname, '../qd-front/static/2123'),
+            filename: '[name].js'
+        },
+        devtool: 'source-map',
+    });
+};
